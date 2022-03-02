33378d3eb65df5cd60566abb254ec2e297cd6e42
Balazs Nyirfa <balazsnyirfa@gmail.com>
2022-03-02 10:53:26 +0100

    add deleteComment route

--- src/comment/routes/index.ts
+++ src/comment/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import { addComment } from "./../controllers/addComment.js";
+import { deleteComment } from './../controllers/deleteComment.js'
 import { authenticateToken } from "./../../global/middlewares/authenticateToken.js";
 
 // create express router
@@ -9,6 +10,10 @@ const router = Router();
 
 router.post("/addComment/:seriesId", authenticateToken, (req: Request, res: Response) => {
   addComment(req, res);
+});
+
+router.post("/deleteComment/:commentId", authenticateToken, (req: Request, res: Response) => {
+  deleteComment(req, res);
 });
 
 export default router;
