94d206106cb05fd1801459a66861027331dff8ae
Balazs Nyirfa <balazsnyirfa@gmail.com>
2022-10-15 17:50:36 +0200

    use helmet before cors to avoid conflict

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,31 @@
-import { env } from 'process';
-import 'dotenv/config'
+import { env } from "process";
+import "dotenv/config";
 import express from "express";
 import compression from "compression";
 import mongoose from "mongoose";
 import ExpressMongoSanitize from "express-mongo-sanitize";
 import cors from "cors";
 import helmet from "helmet";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import { checkMediaDir } from "./global/storage.js";
 // routes
 import userRoute from "./user/routes/index.js";
 import seriesRoute from "./series/routes/Series.js";
-import commentRoute from './comment/routes/index.js';
+import commentRoute from "./comment/routes/index.js";
 import defaultRoutes from "./global/routes/defaultRoutes.js";
 
 // define port
 const port = env.PORT || 3000;
 // init express app
 const app = express();
+// add set of security features
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 // use cors
 const origin = env.ORIGIN || "http://localhost:3000";
 app.use(cors({ origin: origin, credentials: true }));
 // compress every request
 app.use(compression());
-// add set of security features
-app.use(helmet());
 // sanitize user-supplied data to prevent MongoDB Operator Injection
 app.use(ExpressMongoSanitize());
 // parse JSON
@@ -45,8 +46,8 @@ const start = async () => {
     // determine mediaDir on server start
     await checkMediaDir();
     // MongoDB server connection setup
-    const mongoDB = process.env.DB_URI;
-    if (!mongoDB) throw new Error("MongoDB connection URI is missing!");
+    const mongoDB = env.DB_URI;
+    if (!mongoDB) throw new Error("MongoDB connection URI is missing!");    
     mongoose.connect(mongoDB);
     const db = mongoose.connection;
     // Bind connection to error event (to get notification of connection errors)
